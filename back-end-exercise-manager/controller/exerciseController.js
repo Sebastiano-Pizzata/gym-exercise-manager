eaf39814f182fabdbe7f04fcb86f5d2fbf0e7226
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-10-02 15:39:51 +0200

    ADD: show query

--- back-end-exercise-manager/controller/exerciseController.js
+++ back-end-exercise-manager/controller/exerciseController.js
@@ -1,5 +1,8 @@
 import connection from "../data/db.js";
 
+
+//index controllers
+
 function index(req, res) {
     const sql = "SELECT * FROM exercises";
 
@@ -47,8 +50,61 @@ function indexTypes(req, res) {
 
 
 
+//show controller
+
+function show(req, res) {
+    const { id } = req.params;
+
+    const exerciseSql = 'SELECT * FROM exercises WHERE id = ?';
+    const categorySql = `SELECT * FROM exercises JOIN categories ON exercises.id = categories.id WHERE categories.id = ? `;
+    const typeSql = `SELECT * FROM exercises JOIN types ON exercises.type_id = types.id WHERE types.id = ? `;
+
+    //exercise query
+    connection.query(exerciseSql, [id], (exerciseErr, exerciseResult) => {
+        if (exerciseErr) {
+            return res.status(500).json({
+                error: 500,
+                message: "Errore nella query exercise"
+            });
+        }
+        if (exerciseResult.length === 0) {
+            return res.status(404).json({ error: "Esercizio non trovato" });
+        }
+
+        const exercise = exerciseResult[0];
+
+        // category query
+        connection.query(categorySql, [id], (categoryErr, categoryResult) => {
+            if (categoryErr) {
+                return res.status(500).json({
+                    error: 500,
+                    message: "Errore nella query category"
+                });
+            }
+
+            // type query
+            connection.query(typeSql, [id], (typeErr, typeResult) => {
+                if (typeErr) {
+                    return res.status(500).json({
+                        error: 500,
+                        message: "Errore nella query type"
+                    });
+                }
+                res.json({
+                    exercise,
+                    categories: categoryResult,
+                    type: typeResult[0] || null
+                });
+            });
+        });
+    });
+}
+
+
+
 export {
     index,
     indexCategories,
-    indexTypes
+    indexTypes,
+    show
 } 
--- back-end-exercise-manager/routers/exerciseRouter.js
+++ back-end-exercise-manager/routers/exerciseRouter.js
@@ -1,11 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-import { index, indexCategories, indexTypes } from '../controller/exerciseController.js';
+import { index, indexCategories, indexTypes, show } from '../controller/exerciseController.js';
 
 //route -> index
 router.get("/", index)
 router.get("/categories", indexCategories)
 router.get("/types", indexTypes)
+router.get("/:id", show)
 
 export default router
